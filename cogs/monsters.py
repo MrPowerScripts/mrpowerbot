133c891cc71cfbfb2e284023813e2dbe269a36c7
MrPowerScripts <playingwithpowershell@gmail.com>
2020-08-23 15:23:55 +0100

    test monster

--- cogs/monsters.py
+++ cogs/monsters.py
@@ -1,5 +1,6 @@
 import random
 import discord
+import time
 from utils import TEST_CHANNEL
 from discord.ext import commands
 
@@ -7,11 +8,13 @@ class Monster:
   def __init__(self):
     self.hp = random.randint(10, 20)
 
+  def remove_hp(self, amount)
 
 class TestMonster(Monster):
   def __init__(self):
     self.hp = randomm.randint(1, 3)
 
+monster_mash = [Monster, TestMonster]
 
 class Monsters(commands.Cog):
   def __init__(self, bot):
@@ -20,13 +23,24 @@ class Monsters(commands.Cog):
   @commands.Cog.listener()
   async def on_ready(self):
     game_channel = self.bot.get_channel(TEST_CHANNEL)
-    await game_channel.send("monster game initialized")
+    test_message = await game_channel.send("monster game initialized")
     print('Monster game ready!')
+    
+    while True:
+      time.sleep(5)
+      game_channel.send("starting game")
+      monster = random.choice(monster_mash)()
+      game_channel.send(f"monster hp is: {monster.hp}")
+      monster.remove_hp(5)
+      game_channel.send(f"monster hp is: {monster.hp}")
+      
+      time = random.randint(1, 3600)
+      game_channel.send(f"new gammme in: {time}")
+      time.sleep(time)
 
-
-  @commands.Cog.listener()
-  async def on_message(self, message):
-    print(message)
+  # @commands.Cog.listener()
+  # async def on_message(self, message):
+  #   print(message)
 
 def setup(bot):
   bot.add_cog(Monsters(bot))
