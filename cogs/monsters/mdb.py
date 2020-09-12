458ecada11bdf8cee0f2c8984be9b2741a2e1170
MrPowerScripts <playingwithpowershell@gmail.com>
2020-09-12 21:10:25 +0100

    f it

--- cogs/monsters/mdb.py
+++ cogs/monsters/mdb.py
@@ -1,5 +1,10 @@
 import json
-from ..db import conn
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 def save(discord_id, mondata):
   cursor = conn.cursor()
