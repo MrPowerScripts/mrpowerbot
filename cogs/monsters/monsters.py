f5f102b91f31309950fe869cf1e07bd64eefdf25
MrPowerScripts <playingwithpowershell@gmail.com>
2020-09-12 23:43:23 +0100

    prolly gotta be this

--- cogs/monsters/monsters.py
+++ cogs/monsters/monsters.py
@@ -114,7 +114,7 @@ class Monsters(commands.Cog):
     self.battling = False
     self.last_run = int(time.time())
     # solo kills
-    if sum(self.monster_attackers.values()) == 1:
+    if len(self.monster_attackers.values()) == 1:
       attacker = self.monster_attackers.most_common()[0]
       atckr = mdb.load(attacker)
       if not 'solo_kill' in atckr:
