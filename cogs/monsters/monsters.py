d8fc2ecad92c9b41a8a0ece8647a958f7cc77a3c
MrPowerScripts <playingwithpowershell@gmail.com>
2020-09-12 21:52:23 +0100

    dummy

--- cogs/monsters/monsters.py
+++ cogs/monsters/monsters.py
@@ -115,8 +115,7 @@ class Monsters(commands.Cog):
       atckr = mdb.load(attacker)
       if not 'attacks' in atckr:
         atckr['attacks'] = 0
-      else:
-        atckr['attacks'] += self.monster_attackers[attacker]
+      atckr['attacks'] += self.monster_attackers[attacker]
       mdb.save(attacker, atckr)
     await self.monster_message.edit(content=self.mm_formated())
   
