46aaf7ac0644220e430ab7bebedaa92abd078ab6
MrPowerScripts <playingwithpowershell@gmail.com>
2020-12-31 10:04:56 +0000

    whoops

---
+++ cogs/reddit/reddit.py
@@ -0,0 +1,18 @@
+from discord.ext import commands, tasks
+from utils import (
+  ANNOUNCE_CHANNEL,
+  MRPOWER_ROLE,
+)
+
+class Reddit(commands.Cog):
+    def __init__(self, bot):
+    self.bot = bot
+
+    @commands.command()
+    @commands.has_role(MRPOWER_ROLE)
+    async def monprob(self, ctx, title: str, content: str):
+      print(f"title is: {title}")
+      print(f"content is: {content}")
+
+def setup(bot):
+  bot.add_cog(Reddit(bot))
